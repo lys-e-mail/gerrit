cb1411bcf73390414eb330f01ede9c1446fe04ab
Dhruv Srivastava <dhruvsri@google.com>
2024-04-04 11:22:35 +0000

    Ignore Updates
    Original patch:
     diff --git a/polygerrit-ui/app/constants/messages.ts b/polygerrit-ui/app/constants/messages.ts
    new file mode 100644
    --- /dev/null
    +++ b/polygerrit-ui/app/constants/messages.ts
    @@ -0,0 +1,2 @@
    +/**
    + * Copyright 2020 Google LLC
    
    Change-Id: I53138d6d6969ec0e2003ba059f7a4f7a0789e8cc

--- java/com/google/gerrit/server/account/AccountCache.java
+++ java/com/google/gerrit/server/account/AccountCache.java
@@ -1,29 +1,18 @@
 // Copyright (C) 2009 The Android Open Source Project
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-// http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
 
-package com.google.gerrit.server.account;
 
-import com.google.gerrit.common.UsedAt;
-import com.google.gerrit.common.UsedAt.Project;
-import com.google.gerrit.entities.Account;
-import java.util.Map;
-import java.util.Optional;
+
+
+
+
+
 import java.util.Set;
 import org.eclipse.jgit.lib.ObjectId;
 
 /** Caches important (but small) account state to avoid database hits. */
-public interface AccountCache {
+public interface AccountSupport {
   /**
    * Returns an {@code AccountState} instance for the given account ID. If not cached yet the
    * account is loaded. Returns {@link Optional#empty()} if the account is missing.
