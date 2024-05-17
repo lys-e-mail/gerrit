eae5c236648093fe64fa2d3f56349974c983604a
Nitzan Gur-Furman <nitzan@google.com>
2024-05-17 11:46:35 +0000

    Make AccountInputProtoConverter safe
    Release-Notes: skip
    Google-Bug-Id: b/335372403
    Change-Id: I9c189e76cb7751fb66b37d84cc6b35c63159544c

--- java/com/google/gerrit/extensions/api/accounts/AccountInput.java
+++ java/com/google/gerrit/extensions/api/accounts/AccountInput.java
@@ -14,9 +14,12 @@
 
 package com.google.gerrit.extensions.api.accounts;
 
+import com.google.gerrit.common.ConvertibleToProto;
 import com.google.gerrit.extensions.restapi.DefaultInput;
 import java.util.List;
+import java.util.Objects;
 
+@ConvertibleToProto
 public class AccountInput {
   @DefaultInput public String username;
   public String name;
@@ -25,4 +28,24 @@ public class AccountInput {
   public String sshKey;
   public String httpPassword;
   public List<String> groups;
+
+  @Override
+  public boolean equals(Object o) {
+    if (!(o instanceof AccountInput)) {
+      return false;
+    }
+    AccountInput other = (AccountInput) o;
+    return Objects.equals(username, other.username)
+        && Objects.equals(name, other.name)
+        && Objects.equals(displayName, other.displayName)
+        && Objects.equals(email, other.email)
+        && Objects.equals(sshKey, other.sshKey)
+        && Objects.equals(httpPassword, other.httpPassword)
+        && Objects.equals(groups, other.groups);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(username, name, displayName, email, sshKey, httpPassword, groups);
+  }
 }
