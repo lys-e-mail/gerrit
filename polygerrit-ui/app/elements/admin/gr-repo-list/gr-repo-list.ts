808920b9f56407fbb6e52865c3b014a8784d9ad3
Antoine Musso <hashar@free.fr>
2024-06-19 23:28:03 +0200

    Allow hiding read-only repo in the project list
    When we no more use a repository, we change its state to `read-only` and
    prefix its description with `[ARCHIVED]`.  Over time, the repositories
    clutter the UI list on `/admin/repos`.
    
    This change offers a way to only list active repositories by adding a
    checkbox /Hide read only repositories/ next to the filtering input box.
    A new slot "afterFilter" is added to `<gr-list-view>`.
    
    When checked, the call to the `/projects/` REST API is passed
    `state=ACTIVE` which filters out read-only and hidden repositories:
    
      Filter: [________________]  [✔] Hide read only repositories
    
    Screenshot: https://phabricator.wikimedia.org/F55880056
    
    The search project state operator does not accept 'hidden' and would
    throw:
    
      state operator must be either 'active' or 'read-only'.
    
    The hidden state was not added in the project index when that got added
    with 36de336dc1acbdb02c9fc64d85bfb5fe2afedb9b. I have thus made the
    TypeScript types to only accept ACTIVE, READ-ONLY or undefined.
    
    Closes: https://phabricator.wikimedia.org/T180297
    Release-Notes: The project list now allows hidding read only repositories.
    Change-Id: If0790e0132a997e3c041b86e4c083096c8a06b2d

--- polygerrit-ui/app/elements/admin/gr-repo-list/gr-repo-list.ts
+++ polygerrit-ui/app/elements/admin/gr-repo-list/gr-repo-list.ts
@@ -40,6 +40,9 @@ export class GrRepoList extends LitElement {
   @property({type: Object})
   params?: AdminViewState;
 
+  @property({type: Boolean})
+  hideReadOnlyRepos?: Boolean;
+
   @state() offset = 0;
 
   @state() newRepoName = false;
@@ -100,6 +103,16 @@ export class GrRepoList extends LitElement {
         .path=${createAdminUrl({adminView: AdminChildView.REPOS})}
         @create-clicked=${() => this.handleCreateClicked()}
       >
+        <div slot="afterFilter" style="display:inline;">
+          <input
+            id="readOnlyRepos"
+            type="checkbox"
+            .checked=${this.hideReadOnlyRepos}
+            @change=${this.handleToggleReadOnlyRepos}
+          />
+          <label for="readOnlyRepos">Hide read only repositories</label>
+        </div>
+
         <table id="list" class="genericList">
           <tbody>
             <tr class="headerRow">
@@ -175,7 +188,10 @@ export class GrRepoList extends LitElement {
   }
 
   override willUpdate(changedProperties: PropertyValues) {
-    if (changedProperties.has('params')) {
+    if (
+      changedProperties.has('params') ||
+      changedProperties.has('hideReadOnlyRepos')
+    ) {
       this._paramsChanged();
     }
   }
@@ -225,7 +241,8 @@ export class GrRepoList extends LitElement {
     const repos = await this.restApiService.getRepos(
       this.filter,
       this.reposPerPage,
-      this.offset
+      this.offset,
+      this.hideReadOnlyRepos ? RepoState.ACTIVE : undefined
     );
 
     // Late response.
@@ -257,6 +274,10 @@ export class GrRepoList extends LitElement {
   handleCreateClicked() {
     this.createModal?.showModal();
     this.createNewModal?.focus();
+  }
+
+  handleToggleReadOnlyRepos() {
+    this.hideReadOnlyRepos = !this.hideReadOnlyRepos;
   }
 
   // private but used in test
